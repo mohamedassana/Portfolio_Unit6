4a50198b9089efe7c43b3f0a78fce61568d0a4cc
mohemed assana <massana@mihsnyc.org>
2017-01-18 14:47:58 +0000

    added pages

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$("#document").ready(function(){
     //Write all your jQuery code here
         $("button").click(function() {
       $("#like").html("You like this project!");
